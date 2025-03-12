1f54e90cb4d36f13e1f5d8a8e4eb59c07dcbbaa6
deathwear <amrreza786@gmail.com>
2025-03-12 16:19:56 +0530

    datatypes with notes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello");  (this will give a alert popup on brwser side when working in devloper console for js) [we are using node.js not browser]
+
+console.log(3 + 3);
+console.log("Aamir");    // code readability should be high
+
+
+//refer mdn documentation
+//but the original documentation are of ecma
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// String => ""
+// boolean => true/false
+// null  (standalone value)
+/* undefined (var is declared but value is not defined)
+        for e.g: let state;
+            or,  let state = null
+*/
+// symbol (used for the uniqueness )
+// object
+
+console.log(typeof null);  //object
+console.log(typeof undefined);  // undefined
+// typeof is used for checking the data types
+
