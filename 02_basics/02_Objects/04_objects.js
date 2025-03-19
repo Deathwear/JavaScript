f963962920a2288ab338b4e25fff81708e0c1fbd
deathwear <amrreza786@gmail.com>
2025-03-19 17:13:50 +0530

    objet lec-17&17 & JSON api

--- 02_basics/02_Objects/04_objects.js
+++ 02_basics/02_Objects/04_objects.js
@@ -23,12 +23,72 @@ console.log(regularUser.fullname.userfullname.firstname);
 console.log(regularUser.fullname?.userfullname.firstname);
 
 //combining object like combining array
-
 const obj1 = { 1: "a", 2: "b" };
 const obj2 = { 3: "a", 4: "b" }; //lec:17 timestamp: 7:00
 const obj4 = { 5: "a", 6: "b" };
-
 //const obj3 = {obj1, obj2}
-const obj3 = Object.assign({}, obj1, obj2, obj4); //giving empty parenthesis
+//const obj3 = Object.assign({}, obj1, obj2, obj4); //giving empty parenthesis
+
+const obj3 = { ...obj1, ...obj2, ...obj4 };
+
 console.log(obj3);
+
+//when value comes from database (object inside an array)
+
+const users = [
+  {
+    id: 1,
+    email: "h@gmail.com",
+  },
+  {
+    id: 1,
+    email: "h@gmail.com",
+  },
+  {
+    id: 1,
+    email: "h@gmail.com",
+  },
+];
+
+users[1].email;
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser)); //IMPORTANT
+console.log(Object.values(tinderUser)); //IMPORTANT
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+//-------------Lecture:18 (De-structuring & JSON API)---------
+
+const course = {
+  coursename: "js in hindi",
+  price: "999",
+  courseInstructor: "hitesh",
+};
+
+//course.courseInstructor (no need to always use this)
+const { courseInstructor: instructor } = course;
+console.log(instructor);
+
+/* e.g; for de-structuring:
+const navbar = ({ company }) => {};
+navbar((company = "aamir"));
+*/
+
+
+//{
+//  name: "hitesh",
+//  coursename: "js in hindi",
+//  price: "free"
+//}
+
+//many we get api's data in array format also 
+//[
+//  {},
+//  {},
+//  {},
+//]
+
+//check random user me api
 
