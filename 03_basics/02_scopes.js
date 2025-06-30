8b5bc6c90c917f3614f66985936ceb7182a9d377
deathwear <amrreza786@gmail.com>
2025-06-30 18:41:05 +0530

    scoping

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -15,20 +15,44 @@ if (true) {
 
 //------ lec:22 (scope & mini-hoisting) ------
 
-
 function one() {
-    const username = "aamir"
-    console.log("one is executed");
-    
+  const username = "aamir";
+  console.log("one is executed");
 
-    function two() {
-        const website = "youtube"
-        console.log(username);
-        
-    }
-    //console.log(website);
-    two()
-    
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  // console.log(website);
+  two();
 }
 
-one()
+// one()
+
+if (true) {
+  const username = "aamir";
+  if (username === "aamir") {
+    const website = "youtube";
+    console.log(username + website);
+  }
+  // console.log(website);
+  console.log(username); // This will work
+}
+
+//  +++++++++++++++++++ interesting example +++++++++++++++++++++
+
+
+console.log(addone(5)); // 6
+function addone(num) {
+  return num + 1;
+}
+
+
+const addtwo = function (num) {
+  return num + 2;
+};
+addtwo(5); // 7
+
+// we can use addone function before its declaration because of hoisting
+// but we cannot use addtwo function before its declaration because it is a function expression
+
