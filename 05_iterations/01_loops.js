17a832a8d22b570985d918ddd7feb83b4689706b
deathwear <amrreza786@gmail.com>
2025-07-19 19:14:49 +0530

    refactor for loop examples for clarity and consistency

--- 05_iterations/01_loops.js
+++ 05_iterations/01_loops.js
@@ -10,10 +10,40 @@ for (let i = 0; i <= 10; i++) {
 }
 // console.log(element); // This will throw an error because 'element' is not defined outside the loop
 
-for (let i = 0; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) {
   console.log(`Outer loop value: ${i} `);
-  for (let j = 0; j <= 10; j++) {
-    const element = array[j];
+  for (let j = 1; j <= 10; j++) {
+    //console.log(`Inner loop value: ${j} and inner loop ${i}`);
+    console.log( i + '*' + j + ' = ' + (i * j));
+    
      
   }
 }
+
+//for loop with array
+let myArray = ["flash", "batman", "superman", "spiderman"];
+  for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element); 
+
+  }
+
+
+  // break and continue
+for (let i = 1; i <= 20; i++){
+  if (i == 5) {
+    console.log("detected 5, breaking the loop");
+    break; // This will exit the loop when i is 5
+  }
+  console.log(`value of i is: ${i}`);
+  
+}
+// continue
+for (let i = 1; i <= 20; i++){
+  if (i == 5) {
+    console.log("detected 5, skipping this iteration");
+    continue; // This will skip the current iteration when i is 5
+  }
+  console.log(`value of i is: ${i}`);
+  
+} 
