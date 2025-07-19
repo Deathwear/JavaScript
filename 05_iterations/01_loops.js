681ee732eba209f3ae92c4cd56ef44c82dd7e13b
deathwear <amrreza786@gmail.com>
2025-07-19 18:50:12 +0530

    add for loop examples with console output

---
+++ 05_iterations/01_loops.js
@@ -0,0 +1,19 @@
+// ----- Lecture: 27 (for loop) -----
+
+// for loop
+for (let i = 0; i <= 10; i++) {
+  const element = i;
+  if (element == 5) {
+    console.log("5 is the best number");
+  }
+  console.log(element);
+}
+// console.log(element); // This will throw an error because 'element' is not defined outside the loop
+
+for (let i = 0; i <= 10; i++) {
+  console.log(`Outer loop value: ${i} `);
+  for (let j = 0; j <= 10; j++) {
+    const element = array[j];
+     
+  }
+}
