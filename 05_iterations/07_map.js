088de2a0106bce1549d69e71e4a5830b01a687e1
deathwear <amrreza786@gmail.com>
2025-07-21 16:33:11 +0530

    add map examples demonstrating array transformations and chaining

---
+++ 05_iterations/07_map.js
@@ -0,0 +1,16 @@
+// --- Lecture(30): Maps ------
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const newNums = myNumbers.map((num) => {
+  return num + 10;
+});
+
+// chaining of map
+
+const another_nums = myNumbers
+  .map((num) => num * 10)
+  .map((num) => num + 1)
+  .filter((num) => num >= 40);
+
+console.log(newNums);
+console.log(another_nums);
